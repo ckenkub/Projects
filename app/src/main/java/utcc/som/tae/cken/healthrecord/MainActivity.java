7a896b28ad1cba546a4920dbe082fe13ace862eb
masterUNG <phrombutr@gmail.com>
2015-10-08 12:45:54 +0700

    Start project

---
+++ app/src/main/java/utcc/som/tae/cken/healthrecord/MainActivity.java
@@ -0,0 +1,13 @@
+package utcc.som.tae.cken.healthrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
